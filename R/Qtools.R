e9309f9a6b08962c15555602c13254ab674072ff
Marco Geraci <41693576+marco-geraci@users.noreply.github.com>
2023-05-16 10:52:22 +0200

    Add files via upload

--- R/Qtools.R
+++ R/Qtools.R
@@ -861,7 +861,7 @@ attr(val, "stderr") <- stderr
 return(val)
 }
 
-quantile.exact <- function(x, probs = 0.5, level = 0.95){
+qexact <- function(x, probs = 0.5, level = 0.95){
 
 if(any(probs < 0) | any(probs > 1)) stop("Quantile index out of range: probs must be between 0 and 1")
 if(any(is.na(x))) warning("Missing values will be omitted")
@@ -3743,6 +3743,7 @@ if (m.n != 0) {
 	V <- V/n ## CHECK V AND WEIGHTS
 	stds <- sqrt(diag(V))
 	} else {
+	V <- NA
 	stds <- NA
 	warning("Standard error not available")
 	}
@@ -3942,9 +3943,9 @@ addnoise <- function(x, centered = TRUE, B = 0.999)
     return(z)
 }
 
-######################################################################
+##################################################
 # Directional quantile classification
-######################################################################
+##################################################
 
 dqcControl <- function(tau.range = c(0.001, 0.999), nt = 10, ndir = 50, seed = NULL){
 
@@ -4118,6 +4119,377 @@ if (!is.null(cl <- x$call)) {
 
 
 }
+
+
+##################################################
+# Quantile ratio regression
+##################################################
+
+qrr <- function(formula, data, taus, start = "rq", beta = NULL, tsf = "bc", symm = TRUE, dbounded = FALSE, linearize = TRUE, kernel = "Gaussian", maxIter = 10, epsilon = 1e-5, verbose = FALSE, method.rq = "fn", method.nlrq = "L-BFGS-B"){
+
+cl <- match.call()
+
+delta <- 1e-6
+taus <- sort(taus)
+if(length(taus) != 2) stop("Provide only two quantile levels")
+if(diff(taus) == 0) stop("Provide two distinct quantile levels")
+p2 <- taus[1]
+p1 <- taus[2]
+
+mf.old <- mf <- model.frame(formula, data)
+intercept <- attr(terms(mf), "intercept") == 1
+x <- model.matrix(formula, mf)
+xn <- if(intercept) x[,c(-1),drop=FALSE] else x
+y <- model.extract(mf, "response")
+n <- length(y)
+
+if(any(y <= 0)) stop("The response must be strictly positive")
+
+H1 <- quantile(y, probs = p1, names = FALSE)
+
+if(length(start) > 1){
+	# start with user-provided values
+	stopifnot(is.numeric(start))
+	stopifnot(length(start) == n)
+	H2 <- start
+} else {
+	if(start == "rq"){
+		# start with rq
+		fit2 <- try(rq.fit(x = x, y = y, tau = p2, method = "fn"), silent = TRUE)
+		if(inherits(fit2, "try-error")) stop("rq failed with error: ", fit2, "Change start estimate")
+		bhat <- fit2$coef
+		H2 <- x%*%bhat
+		if(any(H2 <= 0)) warning("Some predictions are not strictly positive")
+	}
+
+	if(start == "tsrq"){
+		# start with tsrq
+		fit2 <- try(tsrq(formula, data = data, tau = p2, tsf = tsf, symm = symm, dbounded = dbounded, method = "fn"), silent = TRUE)
+		if(inherits(fit2, "try-error")) stop("tsrq failed. Change start estimate")
+		H2 <- predict(fit2)
+	}
+	
+	if(start == "conquer"){
+		# start with conquer (convolution-type smoothing)
+		fit2 <- try(conquer(X = xn, Y = y, tau = p2, kernel = kernel), silent = TRUE)
+		if(inherits(fit2, "try-error")) stop("conquer failed. with error: ", fit2, "Change start estimate")
+		bhat <- fit2$coef
+		H2 <- x%*%bhat
+		if(any(H2 <= 0)) warning("Some predictions are not strictly positive")
+	}
+
+	if(start == "llqr"){
+		# start with llqr
+		fit2 <- try(llqr(x = xn, y = y, tau = p2), silent = TRUE)
+		if(inherits(fit2, "try-error")) stop("llqr failed. Change start estimate")
+		class(fit2) <- "llqr"
+		H2 <- predict(fit2)
+	}
+}
+
+H2 <- as.numeric(H2)
+
+tt <- colnames(x)
+tt <- gsub(" ", "", tt, fixed = TRUE)
+colnames(x) <- tt
+tt <- tt[tt != "(Intercept)"]
+tt <- if(intercept) c("b1", paste(tt, paste0("b", 2:ncol(x)), sep = "*")) else paste(tt, paste0("b", 1:ncol(x)), sep = "*")
+lp <- paste(tt, collapse = " + ")
+
+if(linearize){
+	ff1 <- ff2 <- update.formula(formula, "z ~ .")
+} else {
+	ff1 <- as.formula(paste("z ~ 1 + exp(", lp, ")"))
+	ff2 <- as.formula(paste("z ~ 1/(1 + exp(", lp, "))"))
+	mf <- data.frame(mf, x)
+}
+
+if(is.null(beta)) {
+	beta <- rep(0, ncol(x))
+	names(beta) <- paste0("b", 1:ncol(x))
+}
+
+if(!is.null(beta)) {
+	stopifnot(length(beta) == ncol(x))
+	names(beta) <- paste0("b", 1:ncol(x))
+}
+
+bhat <- beta
+iter <- 0
+h1 <- h2 <- NULL
+
+while(iter < maxIter){
+
+if(verbose) cat("Iteration", iter + 1, "\n")
+
+if(linearize){
+	z <- y/H2 - 1
+	z[z <= 0] <- delta
+	z1 <- mf$z <- log(z)
+	if(method.rq == "conquer"){
+		fit1 <- conquer(X = xn, Y = mf$z, tau = p1, kernel = kernel)
+		class(fit1) <- "conquer"
+		bhat <- coef(fit1)
+		h1 <- fit1$bandwidth
+	} else {
+		fit1 <- rq(ff1, tau = p1, data = mf, method = method.rq)
+		bhat <- coef(fit1)
+	}
+	zhat <- if(method.rq %in% c("pfn", "conquer")) x%*%bhat else predict(fit1)
+	H1 <- (1 + exp(zhat))*H2
+} else {
+	mf$z <- y/H2
+	fit1 <- nlrq(ff1, tau = p1, data = mf, start = as.list(bhat), method = method.nlrq)
+	bhat <- coef(fit1)
+	H1 <- predict(fit1)*H2
+}
+if(verbose) cat("gamma1", coef(fit1), "\n")
+
+if(linearize){
+	z <- H1/y - 1
+	z[z <= 0] <- delta
+	z2 <- mf$z <- log(z)
+	if(method.rq == "conquer"){
+		fit2 <- conquer(X = xn, Y = mf$z, tau = 1 - p2, kernel = kernel)
+		class(fit2) <- "conquer"
+		bhat <- coef(fit2)
+		h2 <- fit2$bandwidth
+	} else {
+		fit2 <- rq(ff2, tau = 1 - p2, data = mf, method = method.rq)
+		bhat <- coef(fit2)
+	}
+	zhat <- if(method.rq %in% c("pfn", "conquer")) x%*%bhat else predict(fit2)
+	H2 <- H1/(1 + exp(zhat))
+} else {
+	mf$z <- y/H1
+	fit2 <- nlrq(ff2, tau = p2, data = mf, start = as.list(bhat), method = method.nlrq)
+	bhat <- coef(fit2)
+	H2 <- predict(fit2)*H1
+}
+
+if(verbose) cat("gamma2", coef(fit2), "\n")
+
+if(max(abs(coef(fit1) - coef(fit2))) < epsilon) {
+		cat("Algorithm converged", "\n")
+		break
+	}
+iter <- iter + 1
+}
+
+if(iter == maxIter) warning("Algorithm reached maximum number of iterations")
+
+if(linearize){
+	if(method.rq == "conquer"){
+		omega1 <- vcov.conquer(res = fit1$residual, x = x, tau = p1, h = h1, intercept = intercept)
+		omega2 <- vcov.conquer(res = fit2$residual, x = x, tau = 1 - p2, h = h2, intercept = intercept)
+	} else {
+		omega1 <- summary(fit1, se = "nid", covariance = TRUE)$cov
+		omega2 <- summary(fit2, se = "nid", covariance = TRUE)$cov
+	}
+} else {
+	omega1 <- summary(fit1)$cov
+	omega2 <- summary(fit2)$cov
+}
+
+names(bhat) <- colnames(x)
+ans <- list(call = cl, formula = formula, coef = bhat, p1 = p1, p2 = p2, omega1 = omega1, omega2 = omega2, ff1 = ff1, ff2 = ff2, data = mf.old, x = x, y = y, H1 = as.numeric(H1), H2 = as.numeric(H2), method.rq = method.rq, intercept = intercept, taus = taus, start = start, beta = beta, tsf = tsf, symm = symm, dbounded = dbounded, linearize = linearize, kernel = kernel, maxIter = maxIter, epsilon = epsilon, verbose = verbose, method.rq = method.rq, method.nlrq = method.nlrq)
+attr(ans, "linearize") <- linearize
+class(ans) <- "qrr"
+return(ans)
+}
+
+coef.qrr <- coefficients.qrr <- function(object, ...){
+
+object$coef
+
+}
+
+predict.qrr <- function(object, newdata, na.action = na.pass, type = "response", ...){
+
+tau <- object$tau
+nq <- length(tau)
+betahat <- object$coef
+
+if(missing(newdata)) {x <- object$x} else {
+	objt <- terms(object)
+	Terms <- delete.response(objt)
+	m <- model.frame(Terms, newdata, na.action = na.action, 
+		xlev = object$levels)
+	if (!is.null(cl <- attr(Terms, "dataClasses"))) 
+		.checkMFClasses(cl, m)
+	x <- model.matrix(Terms, m, contrasts.arg = object$contrasts)
+}
+
+linpred <- x %*% betahat
+
+if(type == "link"){
+	return(linpred)
+}
+
+if(type == "response"){
+	return(1 + exp(linpred))
+}
+
+
+}
+
+qprime <- function(bhat, object) {
+	x <- object$x
+	y <- object$y
+	p1 <- object$p1
+	p2 <- object$p2
+	H1 <- object$H1
+	H2 <- object$H2
+	
+	n <- length(y)
+	p <- ncol(x)
+	h <- max(((log(n)+p)/n)^0.4,0.05)
+	delta <- 1e-6
+	
+	for(j in 1:100) {
+		H2 <- H1/(1 + exp(x%*%bhat))
+		H1 <- H2*(1 + exp(x%*%bhat))
+	}
+
+	# FLAG <- TRUE
+	# iter <- 0
+	# while(FLAG){
+		# H2.n <- H1/(1 + exp(x%*%bhat))
+		# H1.n <- H2*(1 + exp(x%*%bhat))
+		# eps <- max(abs(c(H1.n, H2.n) - c(H1, H2)))
+		# H1 <- H1.n
+		# H2 <- H2.n
+		# if(eps < 1e-6){
+			# FLAG <- FALSE
+		# } else {
+			# iter <- iter + 1
+			# if(iter > 99) FLAG <- FALSE
+		# }
+	# }
+
+	z <- y/H2-1
+	z[z<=0] <- delta
+	res <- log(z)-x%*%bhat 
+	der <- pnorm(-res/h)-p1
+	ans <- t(x)%*%der/n
+
+	z <- H1/y-1
+	z[z<=0] <- delta
+	res <- log(z)-x%*%bhat 
+	der <- pnorm(-res/h)-p2
+	ans <- ans + t(x)%*%der/n
+	return(ans)
+}
+
+vcov.qrr <- function(object, method = "approximate", R = 200, update = TRUE, ...){
+
+ans <- NULL
+
+if(method == "approximate"){
+	stopifnot(attr(object, "linearize"))
+
+	p1 <- object$p1
+	p2 <- object$p2
+	y <- object$y
+	n <- length(y)
+	H1 <- object$H1
+	H2 <- object$H2
+	R <- H1/H2
+
+	ja <- jacobian(qprime, x = object$coef, object = object)
+	ans <- solve(ja)/n
+}
+
+if(method == "boot"){
+	if(update){
+		f <- function(data, inds, object) update(object, data = data[inds,])$coef
+		B <- boot::boot(object$data, f, R = R, stype = "i", parallel = "no", object = object)$t
+	} else {
+		f <- function(data, inds, object) {
+			object$data <- data[inds,]
+			do.call(qrr, object[match(names(object$call)[-c(1)], names(object))])$coef
+		}
+		B <- boot::boot(object$data, f, R = R, stype = "i", parallel = "no", object = object)$t
+	}
+	ans <- var(B)
+}
+
+return(ans)
+
+}
+
+summary.qrr <- function(object, se = "approximate", R = 200, update = TRUE, ...){
+
+V <- vcov(object, method = se, R = R, update = update)
+se <- sqrt(diag(V))
+n <- length(object$y)
+bhat <- coef(object)
+p <- length(bhat)
+
+object$tTable <- data.frame(bhat, se, coef(object)/se, 2*(1 - pt(abs(coef(object)/se), df = n - p)))
+names(object$tTable) <- c("Estimate", "Std.Err", "t value", "Pr(>|t|)")
+
+class(object) <- "summary.qrr"
+
+return(object)
+}
+
+
+print.qrr <- function(x, ...){
+
+if (!is.null(cl <- x$cl)) {
+	cat("call:\n")
+	dput(cl)
+	cat("\n")
+}
+
+coef <- x$coef
+cat("\nQuantile ratio regression", paste(x$p1,x$p2,sep=":"), "\n")
+cat("\nCoefficients linear predictor:\n")
+print(coef, ...)
+nobs <- length(x$y)
+p <- ncol(x$x)
+rdf <- nobs - p
+cat("\nDegrees of freedom:", nobs, "total;", rdf, "residual\n")
+}
+
+print.summary.qrr <- function(x, ...){
+
+if (!is.null(cl <- x$cl)) {
+	cat("call:\n")
+	dput(cl)
+	cat("\n")
+}
+
+cat("\nQuantile ratio regression", paste(x$p1,x$p2,sep=":"), "\n")
+cat("\nCoefficients linear predictor:\n")
+printCoefmat(x$tTable, ...)
+nobs <- length(x$y)
+p <- ncol(x$x)
+rdf <- nobs - p
+cat("\nDegrees of freedom:", nobs, "total;", rdf, "residual\n")
+
+}
+
+predict.llqr <- function(object){
+	return(object$ll_est)
+}
+
+coef.conquer <- function(x){
+	return(x$coeff)
+}
+
+vcov.conquer <- function(res, x, tau, h, intercept){
+	n <- length(res)
+	xn <- if(intercept) x[,c(-1),drop=FALSE] else x
+	Wh <- as.vector(pnorm(-res/h) - tau)^2
+	Stau <- crossprod(sweep(x, 1, Wh, "*"), x)/n
+	Dh <- crossprod(sweep(x, 1, dnorm(res/h), "*"), x)/(n*h)
+	Dhinv <- solve(Dh)
+	V <- Dhinv %*% Stau %*% Dhinv/n
+	return(V)
+}
+
 
 ##################################################
 ### Khmaladze and other tests
